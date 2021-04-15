91ee26197d6b8fa1a278c083cfc0272bf6376da8
burdensome <gudejrwkd@gmail.com>
2021-04-15 18:48:55 +0900

    10_3_practice change

--- 10_3_practice_chapter/Lecture.h
+++ 10_3_practice_chapter/Lecture.h
@@ -11,7 +11,7 @@ private:
 	//Teacher teacher;
 	//std::vector<Student> students;
 
-	Teacher teacher;
+	Teacher *teacher;
 	std::vector<Student*> students;	//여기 설명이 정확히 이해가 가지 않았는데.. 벡터를 다시 보고 돌아와하나?
 
 public:
@@ -25,11 +25,11 @@ public:
 		// do NOT DELTE STUDENTS
 	}
 
-	void assignTeacher(const Teacher* teacher_input)
+	void assignTeacher(Teacher* teacher_input)
 	{
 		std::cout << "teacher_input : " << *(teacher_input) << std::endl;
 		std::cout << "teacher_address : " << (teacher_input) << std::endl;
-		teacher = *(teacher_input);
+		teacher = teacher_input;
 	}	
 
 	void registerStudent(Student * student_input)
@@ -43,14 +43,14 @@ public:
 		std::cout << m_name << " Study " << std::endl << std::endl;
 
 		for (auto& element : students)	// Note : 'auto element' doesn't work, ampersand없이 아래처럼 선언하면 값이 업데이트가 안됨.
-			element->setIntel(element->getIntel() + 1);
+			(*element).setIntel(element->getIntel() + 1);//setIntel(element->getIntel() + 1);
 	}
 
 	friend std::ostream& operator << (std::ostream& out, const Lecture& lecture)
 	{
 		out << "Lecture name : " << lecture.m_name << std::endl;
 
-		out << lecture.teacher << std::endl;
+		out << *(lecture.teacher) << std::endl;
 		for (auto element : lecture.students)
 			out << *element << std::endl;
 
